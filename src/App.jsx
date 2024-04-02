ef8a7b1ae2f6db76b6c855fc40b89af8bc187835
Andres Flores <andresflo883@gmail.com>
2024-04-02 07:37:43 -0400

    added hashrouter

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, HashRouter } from 'react-router-dom';
 import { Route } from 'react-router-dom';
 import { Routes } from 'react-router-dom';
 import Navigation from './components/avixa/Navigation';
@@ -12,12 +12,18 @@ export default function App() {
 
   return (
     <>
-      <BrowserRouter>
+      <HashRouter>
         <h1>Component Library</h1>
         <Routes>
+          <Route path="" element={<Navigation />} />
+          <Route path="component-library/avixa" element={<Navigation />} />
           <Route path="avixa" element={<Navigation />} />
+          <Route path="sss" element={<Navigation />} />
+
+
+
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </>
   );
 }
