8da90e8443610f19a62befe35b0a9f688902253e
Andres Flores <andresflo883@gmail.com>
2024-04-03 07:57:04 -0400

    removed some padding in the vista nav component

--- src/components/modal/Modal.jsx
+++ src/components/modal/Modal.jsx
@@ -3,35 +3,35 @@ import './modal.scss';
 import { RxCross1 } from 'react-icons/rx';
 import { Link } from 'react-router-dom';
 
-
 const Modal = () => {
   return (
     <div>
-
       <Link to="/">
-      
-      <div className='back-link'>Go back</div>
+        <div className="back-link">Go back</div>
       </Link>
-    <div className="modal-component-wrapper">
-      {/* Modal */}
-      <div className="modal-wrapper">
-        <div className="top-modal">
-          <div style={{ display: 'flex', gap: '0.5rem', alignItems: 'center' }}>
-            <div className="hand-emoji">👋</div>
-            <p className="modal-header">Here is the modal's title!</p>
+      <div className="modal-component-wrapper">
+        {/* Modal */}
+        <div className="modal-wrapper">
+          <div className="top-modal">
+            <div
+              style={{ display: 'flex', gap: '0.5rem', alignItems: 'center' }}
+            >
+              <div className="hand-emoji">👋</div>
+              <p className="modal-header">Here is the modal's title!</p>
+            </div>
+            <RxCross1 className="cross-icon" />
           </div>
-          <RxCross1 className="cross-icon" />
-        </div>
-        <div className="blue-wrapper"></div>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ducimus delectus iure praesentium!
-        </p>
-        <div className="btn-wrapper">
-          <button className="no-thanks-btn">No, thanks</button>
-          <button className="get-started-btn">Get started</button>
+          <div className="blue-wrapper"></div>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ducimus
+            delectus iure praesentium!
+          </p>
+          <div className="btn-wrapper">
+            <button className="no-thanks-btn">No, thanks</button>
+            <button className="get-started-btn">Get started</button>
+          </div>
         </div>
       </div>
-    </div>
     </div>
   );
 };
