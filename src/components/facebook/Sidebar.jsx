5cc0cbe7976f8d2f9ff05395d6e3effde91f2ea1
Andres Flores <andresflo883@gmail.com>
2024-04-19 15:25:58 -0400

    added marketplace component; git push origin main

--- src/components/facebook/Sidebar.jsx
+++ src/components/facebook/Sidebar.jsx
@@ -3,36 +3,66 @@ import './facebook.scss';
 import { IoIosSettings } from 'react-icons/io';
 import { IoStorefront } from 'react-icons/io5';
 import { FaBell } from 'react-icons/fa';
-import { MdAllInbox } from "react-icons/md";
-
+import { MdAllInbox } from 'react-icons/md';
+import { Link } from 'react-router-dom';
+import { GoPlus } from 'react-icons/go';
+import { FiPlus } from 'react-icons/fi';
 const Sidebar = () => {
   return (
-    <div className="facebook-wrapper">
-      <div className="marketplace-sidebar">
-        <div className="marketplace-top-bar">
-          <h1>Marketplace</h1>
-          <IoIosSettings className='marketplace-icon'/>
+    <>
+      <Link to="/">
+        <div className="back-link-wrapper">
+          <div className="back-link">Go back</div>
         </div>
-        <div>
-          <input type='text' className='search' placeholder='Search Marketplace' />
-        </div>
+      </Link>
+      <div className="facebook-wrapper">
+        <div className="marketplace-sidebar">
+          <div className="marketplace-top-bar">
+            <h1>Marketplace</h1>
+            <div className="marketplace-icon-wrapper">
+              <IoIosSettings className="marketplace-icon" />
+            </div>
+          </div>
+          <div>
+            <input
+              type="text"
+              className="search"
+              placeholder="Search Marketplace"
+            />
+          </div>
 
-        <div className='marketplace-select'>
-          <IoStorefront className='marketplace-icon'/>
-          <div>Browse all</div>
-        </div>
+          <div className="marketplace-select">
+            <div className="browse-all marketplace-icon-wrapper">
+              <IoStorefront className="marketplace-icon" />
+            </div>
+            <div>Browse all</div>
+          </div>
 
-        <div className='marketplace-select'>
-          <FaBell className='marketplace-icon'/>
-          <div>Notifications</div>
-        </div>
+          <div className="marketplace-select">
+            <div className="marketplace-icon-wrapper">
+              <FaBell className="marketplace-icon" />
+            </div>
+            <div>Notifications</div>
+          </div>
 
-        <div className='marketplace-select'>
-          <MdAllInbox className='marketplace-icon'/>
-          <div>Inbox</div>
+          <div className="marketplace-select">
+            <div className="marketplace-icon-wrapper">
+              <MdAllInbox className="marketplace-icon" />
+            </div>
+
+            <div>Inbox</div>
+          </div>
+
+          <button className="create-listing-btn">
+            <FiPlus className="create-icon" />
+            Create new listing
+          </button>
+          <button className="create-multiple-listings-btn">
+            Create multiple listings
+          </button>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
